702cfe292d9bf339cb4d22996bed70c94740cf6e
王亚强 <2259196468@qq.com>
2018-05-29 17:19:00 +0800

    啦啦啦

---
+++ itwang-spring-boot-starter-autoconfigurer/src/main/java/com/itwang/starter/HelloProperties.java
@@ -0,0 +1,25 @@
+package com.itwang.starter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "itwang.hello")
+public class HelloProperties {
+    private String prefix;
+    private String suffix;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+}
---
+++ itwang-spring-boot-starter-autoconfigurer/src/main/java/com/itwang/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.itwang.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication //web应用才生效
+@EnableConfigurationProperties(HelloProperties.class)
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    HelloProperties helloProperties;
+    //需要将HelloService注入到容器中
+    @Bean
+    public HelloService helloService(){
+        HelloService helloService = new HelloService();
+        helloService.setHelloProperties(helloProperties);
+        return helloService;
+    }
+}
